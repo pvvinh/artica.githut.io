a649dae149e36af86773d45c02a61915e28b6a8d
Vinh Phan <vinh.phan@eiu.edu.vn>
2023-06-01 01:14:08 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,23 @@
+﻿ $(function(){
+  
+	 new WOW().init();
+})  
+
+$(function(){
+	khoimonan = $('.nhieumon').isotope({
+		itemSelector: '.motmon',
+		layoutMode: 'masonry'
+	});
+	khoimonan.imagesLoaded().progress(function() {
+		khoimonan.isotope('layout');
+	});
+
+	$('.tieudect a').click(function (){
+
+		dulieu = $(this).data('monan');
+		// console.log(dulieu);
+
+		khoimonan.isotope({ filter: dulieu});
+		return false;
+	});
+})
